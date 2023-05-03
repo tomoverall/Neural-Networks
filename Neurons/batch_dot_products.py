212e81da46f446e4db250c298c592d0c11128ab3
Tom Overall <58610843+tomoverall@users.noreply.github.com>
2023-05-03 16:42:52 -0400

    Add files via upload

---
+++ Neurons/batch_dot_products.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+inputs = [[1, 2, 3, 2.5], [2.0, 5.0, -1.0, 2.0], [-1.5, 2.7, 3.3, -0.8]]
+weights = [
+    [0.2, 0.8, -0.5, 1.0],
+    [0.5, -0.91, 0.26, -0.5],
+    [-0.26, -0.27, 0.17, 0.87],
+]
+biases = [2.0, 3.0, 0.5]
+# remember that the bias is a vector here so it adds to each neuron column-wise in the matrix.
+outputs = np.dot(inputs, np.array(weights).T) + biases
+print(outputs)
