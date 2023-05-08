07861d77b425641d6c492142406de4efb220de16
Tom Overall <58610843+tomoverall@users.noreply.github.com>
2023-05-08 16:20:43 -0400

    Update Layer_Dense.py
    Now includes loss

--- Layers/Layer_Dense.py
+++ Layers/Layer_Dense.py
@@ -3,6 +3,8 @@ import nnfs
 import matplotlib.pyplot as plt
 from nnfs.datasets import spiral_data
 
+nnfs.init()
+
 
 class Layer_Dense:
     def __init__(self, n_inputs, n_neurons):
@@ -19,14 +21,70 @@ class Layer_Dense:
         self.output = np.dot(inputs, self.weights) + self.biases
 
 
-# Create dataset
+class Activation_ReLU:
+    def forward(self, inputs):
+        self.output = np.maximum(0, inputs)
+
+
+class Activation_Softmax:
+    def forward(self, inputs):
+        # get unnormalized probabilities
+        exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+        # normalize them for each sample
+        probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+        self.output = probabilities
+
+
+class Loss:
+    def calculate(self, output, y):
+        # calculate sample losses
+        sample_losses = self.forward(output, y)
+        # calculate mean loss
+        data_loss = np.mean(sample_losses)
+        return data_loss
+
+
+class Loss_CategoricalCrossentropy(Loss):
+    def forward(self, y_pred, y_true):
+        # number of samples in a batch
+        samples = len(y_pred)
+        # clip data to prevent division by 0
+        # clip both sides to not drag mean towards any value
+        y_pred_clipped = np.clip(y_pred, 1e-7, 1 - 1e-7)
+        # probabilities for target values
+        # only if categorical labels
+        if len(y_true.shape) == 1:
+            correct_confidences = y_pred_clipped[range(samples), y_true]
+        # mask values - only for one-hot encoded labels
+        elif len(y_true.shape) == 2:
+            correct_confidences = np.sum(y_pred_clipped * y_true, axis=1)
+        # losses
+        negative_log_likelihoods = -np.log(correct_confidences)
+        return negative_log_likelihoods
+
+
 X, y = spiral_data(samples=100, classes=3)
 
-# Create Dense layer with 2 input features and 3 output values
 dense1 = Layer_Dense(2, 3)
 
-# Perform a forward pass of our training data through this layer
+activation1 = Activation_ReLU()
+
+dense2 = Layer_Dense(3, 3)
+
+activation2 = Activation_Softmax()
+
 dense1.forward(X)
 
-# Let's see output of the first few samples:
-print(dense1.output[:5])
+activation1.forward(dense1.output)
+
+dense2.forward(activation1.output)
+
+activation2.forward(dense2.output)
+
+print(activation2.output[:5])
+
+loss_function = Loss_CategoricalCrossentropy()
+
+loss = loss_function.calculate(activation2.output, y)
+
+print("Loss: ", loss)
