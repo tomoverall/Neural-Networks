c834e0d61b3ea7e8253623fccd6e763f66803402
Tom Overall <58610843+tomoverall@users.noreply.github.com>
2023-05-15 17:57:02 -0400

    Update Layer_Dense.py

--- Layers/Layer_Dense.py
+++ Layers/Layer_Dense.py
@@ -20,6 +20,11 @@ class Layer_Dense:
         # Calculate output values from inputs, weights and biases
         self.output = np.dot(inputs, self.weights) + self.biases
 
+    def backward(self, dvalues):
+        self.dweights = np.dot(self.dinputs.T, dvalues)
+        self.dbiases = np.sum(dvalues, axis=0, keepdims=True)
+        self.dinputs = np.dot(dvalues, self.weights.T)
+
 
 class Activation_ReLU:
     def forward(self, inputs):
