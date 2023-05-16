5e823791023b7742c1d29c969c5bf1933606facb
Tom Overall <58610843+tomoverall@users.noreply.github.com>
2023-05-16 14:57:46 -0400

    Update Activation_Softmax.py

--- Activation Functions/Activation_Softmax.py
+++ Activation Functions/Activation_Softmax.py
@@ -5,6 +5,32 @@ from nnfs.datasets import spiral_data
 nnfs.init()
 
 
+class Activation_Softmax:
+    def forward(self, inputs):
+        # get unnormalized probabilities
+        exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+        # normalize them for each sample
+        probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+        self.output = probabilities
+
+    def backward(self, dvalues):
+        # create uninitialized array
+        self.dinputs = np.empty_like(dvalues)
+
+        # enumerate outputs and gradients
+        for index, (single_output, single_dvalues) in enumerate(
+            zip(self.output, dvalues)
+        ):
+            # flatten output array
+            single_output = single_output.reshape(-1, 1)
+            # calculate jacobian matrix of the output
+            jacobian_matrix = np.diagflat(single_output) - np.dot(
+                single_output, single_output.T
+            )
+            # calculate sample-wise gradient and add it to the array of sample gradients
+            self.dinputs[index] = np.dot(jacobian_matrix, single_dvalues)
+
+
 # Dense layer
 class Layer_Dense:
     # Layer initialization
@@ -25,5 +51,4 @@ class Layer_Dense:
 
 class Activation_ReLU:
     def forward(self, inputs):
-        # here we are using the maximum function instead of a for-loop to create the ReLU
         self.output = np.maximum(0, inputs)
